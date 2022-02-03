d116c9c0dbb9d1f129c5dca6e90e44aefdb3fe65
Ramon Puig Treserres <raypuig@gmail.com>
2022-02-03 13:33:38 +0100

    act

---
+++ codi/Render_META_COVID.R
@@ -0,0 +1,12 @@
+
+### Generació d'informe
+
+
+
+
+rmarkdown::render("codi/met_lect_prep_anal.Rmd", 
+                  output_file = here::here("resultats",paste0("Informe_Meta_Analisi_COVID_",Sys.Date()))
+                  
+                  )
+
+
